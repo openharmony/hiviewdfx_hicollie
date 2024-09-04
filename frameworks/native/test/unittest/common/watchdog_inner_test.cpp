0f83bac7e16f83c5d7dc2b12f30bb90b1902dabc
hhl <yinhuiling@huawei.com>
2024-09-04 17:22:38 +0800

    fix issues
    Signed-off-by: hhl <yinhuiling@huawei.com>

--- frameworks/native/test/unittest/common/watchdog_inner_test.cpp
+++ frameworks/native/test/unittest/common/watchdog_inner_test.cpp
@@ -137,7 +137,6 @@ HWTEST_F(WatchdogInnerTest, WatchdogInnerTest_FetchNextTask_001, TestSize.Level1
         delay, interval, isOneshot));
     ASSERT_GT(id, 0);
     WatchdogInner::GetInstance().isNeedStop_.store(true);
-    printf("FetchNextTask=%llu", WatchdogInner::GetInstance().FetchNextTask(now, task));
     ASSERT_EQ(WatchdogInner::GetInstance().FetchNextTask(now, task), 60000);
     WatchdogInner::GetInstance().isNeedStop_.store(false);
     WatchdogTask task1;
